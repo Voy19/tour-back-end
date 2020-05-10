01d0a1e1dc72bc68da074246fe7fa744887e77fa
Voy19 <denys.voytov@gmail.com>
2020-05-10 17:06:18 +0300

    added assets

---
+++ assets/precache-manifest.0d71fd9098a418d7a6401d1236deb735.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d4c25cf439c82f9dfb564c35cfd8ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1c094adba61bd0c526f",
+    "url": "/static/css/main.571a5c47.chunk.css"
+  },
+  {
+    "revision": "c0530f46eac08c5c0733",
+    "url": "/static/js/2.aa34ba3d.chunk.js"
+  },
+  {
+    "revision": "2d1f0ed21c5c95e3a2cb212dbd2653e2",
+    "url": "/static/js/2.aa34ba3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c094adba61bd0c526f",
+    "url": "/static/js/main.1f7edbfd.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  },
+  {
+    "revision": "34efef1b4b74e927a01bd7e2b720e744",
+    "url": "/static/media/1.34efef1b.jpg"
+  },
+  {
+    "revision": "ff143395dabba8ac4ef073defce90649",
+    "url": "/static/media/2.ff143395.jpg"
+  },
+  {
+    "revision": "c5309a9a1fdfb2b4ae7037d203ae7a4f",
+    "url": "/static/media/3.c5309a9a.jpg"
+  },
+  {
+    "revision": "62954f1175222321de84279233f4d901",
+    "url": "/static/media/4.62954f11.jpg"
+  },
+  {
+    "revision": "c6ce7b0413ad0e47fda1938ed9ec99a4",
+    "url": "/static/media/bg1.c6ce7b04.jpg"
+  },
+  {
+    "revision": "b80b5bc67e24e752cb77bf4daf34883f",
+    "url": "/static/media/doc.b80b5bc6.png"
+  },
+  {
+    "revision": "841c6f250742ee180707fc9ec503bf52",
+    "url": "/static/media/img0.841c6f25.jpg"
+  },
+  {
+    "revision": "7d9f94ce522c60eec9a4fd4ee0bf32e6",
+    "url": "/static/media/logo-coral-travel.7d9f94ce.svg"
+  },
+  {
+    "revision": "cfb8dc9e24c359fd1239470a90086866",
+    "url": "/static/media/main.cfb8dc9e.jpg"
+  },
+  {
+    "revision": "143242ae365e15aecef51ba6a9be803f",
+    "url": "/static/media/map.143242ae.png"
+  }
+]);
